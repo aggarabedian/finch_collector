bd15055fd1162c731dbfb328bcc630ab76029a4f
aggarabedian <abrahamgarabedian@gmail.com>
2021-08-17 13:41:51 -0700

    Adds context to /records route

---
+++ main_app/db/create_records.sql
@@ -0,0 +1,28 @@
+INSERT INTO main_app_record(title, img, artist, year, created_at)
+  VALUES
+    ('Revolver', 'https://upload.wikimedia.org/wikipedia/en/thumb/e/ec/Revolver_%28album_cover%29.jpg/220px-Revolver_%28album_cover%29.jpg', 'The Beatles', 1966, '2021-08-16 13:49:24');
+
+INSERT INTO main_app_record(title, img, artist, year, created_at)
+  VALUES
+    ('Nashville Skyline', 'https://upload.wikimedia.org/wikipedia/en/thumb/9/93/Bob_Dylan_-_Nashville_Skyline.jpg/220px-Bob_Dylan_-_Nashville_Skyline.jpg', 'Bob Dylan', 1969, '2021-08-16 13:49:24');
+
+INSERT INTO main_app_record(title, img, artist, year, created_at)
+  VALUES
+    ('#1 Record', 'https://upload.wikimedia.org/wikipedia/en/5/57/Big_Star_-1_Record.jpg', 'Big Star', 1972, '2021-08-16 13:49:24');
+
+INSERT INTO main_app_record(title, img, artist, year, created_at)
+  VALUES
+    ('Songs of Leonard Cohen', 'https://upload.wikimedia.org/wikipedia/en/thumb/4/4c/SongsOfLeonardCohen.jpeg/220px-SongsOfLeonardCohen.jpeg', 'Leonard Cohen', 1967, '2021-08-16 13:49:24');
+
+INSERT INTO main_app_record(title, img, artist, year, created_at)
+  VALUES
+    ('Led Zeppelin IV', 'https://upload.wikimedia.org/wikipedia/en/thumb/2/26/Led_Zeppelin_-_Led_Zeppelin_IV.jpg/220px-Led_Zeppelin_-_Led_Zeppelin_IV.jpg', 'Led Zeppelin', 1971, '2021-08-16 13:49:24');
+
+INSERT INTO main_app_record(title, img, artist, year, created_at)
+  VALUES
+    ('Plastic Ono Band', 'https://upload.wikimedia.org/wikipedia/en/thumb/a/a4/JLPOBCover.jpg/220px-JLPOBCover.jpg', 'John Lennon', 1970, '2021-08-16 13:49:24');
+
+INSERT INTO main_app_record(title, img, artist, year, created_at)
+  VALUES
+    ('Pet Sounds', 'https://upload.wikimedia.org/wikipedia/en/thumb/b/bb/PetSoundsCover.jpg/220px-PetSoundsCover.jpg', 'The Beach Boys', 1966, '2021-08-16 13:49:24');
+
--- main_app/views.py
+++ main_app/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.views import View
 from django.http import HttpResponse
 from django.views.generic.base import TemplateView
+from .models import Record
 
 # Create your views here.
 
@@ -13,3 +14,8 @@ class About(TemplateView):
 
 class Records(TemplateView):
   template_name = "record_list.html"
+
+  def get_context_data(self, **kwargs):
+    context = super().get_context_data(**kwargs)
+    context["records"] = Record.objects.all()
+    return context
